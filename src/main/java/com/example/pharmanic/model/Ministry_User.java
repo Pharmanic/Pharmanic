3b92d7c0078051635cda7b6789184d9a4211f78c
AnupamaTK <swathathsarani@gmail.com>
2020-05-20 22:30:21 +0530

    User Level Models
     with Table per class inheritance

---
+++ src/main/java/com/example/pharmanic/model/Ministry_User.java
@@ -0,0 +1,15 @@
+package com.example.pharmanic.model;
+import lombok.Data;
+
+import javax.persistence.Entity;
+
+@Data
+@Entity
+public class Ministry_User extends User {
+
+
+    public Ministry_User() {
+    }
+
+
+}
