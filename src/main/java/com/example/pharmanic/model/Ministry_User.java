72a178fd6a12da558a9e1b52b4ba9d10890f283f
AnupamaTK <swathathsarani@gmail.com>
2020-05-24 16:26:10 +0530

    Correction

--- src/main/java/com/example/pharmanic/model/Ministry_User.java
+++ src/main/java/com/example/pharmanic/model/Ministry_User.java
@@ -17,7 +17,7 @@ public class Ministry_User extends User {
     public Ministry_User() {
     }
 
-    public Ministry_User(Long nic, String first_name, String last_name, String tel_no, String email, String address, String password, Integer status, Integer role, Ministry ministry) {
+    public Ministry_User(String nic, String first_name, String last_name, String tel_no, String email, String address, String password, Integer status, Integer role, Ministry ministry) {
         super(nic, first_name, last_name, tel_no, email, address, password, status, role);
         this.ministry = ministry;
     }
