d33ed97394fbcbc0c14ba2dd53357aa2132bc4f5
Kaumadie Kariyawasam <kkawmadie@gmail.com>
2020-05-29 23:59:09 +0530

    create some entity class

---
+++ src/main/java/com/example/pharmanic/model/Rdhs_Track.java
@@ -0,0 +1,29 @@
+package com.example.pharmanic.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+public class Rdhs_Track {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int track_id;
+    private Date date;
+    private String start_point;
+    private String end_point;
+
+
+    @ManyToOne
+    @JoinColumn(name = "reg_no", nullable = false, referencedColumnName = "reg_no")
+    private Rdhs rdhs;
+
+    @OneToMany
+    @JoinColumn(name = "vehicle_no", nullable = false, referencedColumnName = "vehicle_no")
+    private Rdhs_Vehicle rdhs_vehicle;
+
+    @OneToMany
+    @JoinColumn(name = "nic", nullable = false, referencedColumnName = "nic")
+    private Rdhs_Driver rdhs_driver;
+
+
+
+}
