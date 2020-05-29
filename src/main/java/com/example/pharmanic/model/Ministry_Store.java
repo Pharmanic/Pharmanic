9cac544f3d771c2311ae2ba1d5a0e071c3706f5b
AnupamaTK <swathathsarani@gmail.com>
2020-05-29 14:05:55 +0530

    Ministry Store Model changed

--- src/main/java/com/example/pharmanic/model/Ministry_Store.java
+++ src/main/java/com/example/pharmanic/model/Ministry_Store.java
@@ -9,6 +9,12 @@ import javax.persistence.*;
 public class Ministry_Store {
     private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long m_store_id;
 
+    private String name;
+
+    private String email;
+
+    private String tel_no;
+
     private String location;
 
     private Integer total_storage;
@@ -16,17 +22,20 @@ public class Ministry_Store {
     private Integer available_storage;
 
     @OneToOne
-    @JoinColumn(name = "nic",referencedColumnName = "nic")
+    @JoinColumn(name = "ministry_store_incharge",referencedColumnName = "nic")
     private Ministry_Store_User ministry_store_incharge;
 
     public Ministry_Store() {
     }
 
-    public Ministry_Store(Long m_store_id, String location, Integer total_storage, Integer avilable_storage, Ministry_Store_User ministry_store_incharge) {
+    public Ministry_Store(Long m_store_id, String name, String email, String tel_no, String location, Integer total_storage, Integer available_storage, Ministry_Store_User ministry_store_incharge) {
         this.m_store_id = m_store_id;
+        this.name = name;
+        this.email = email;
+        this.tel_no = tel_no;
         this.location = location;
         this.total_storage = total_storage;
-        this.available_storage = avilable_storage;
+        this.available_storage = available_storage;
         this.ministry_store_incharge = ministry_store_incharge;
     }
 
@@ -48,6 +57,38 @@ public class Ministry_Store {
 
     public void setMinistry_store_incharge(Ministry_Store_User ministry_store_incharge) {
         this.ministry_store_incharge = ministry_store_incharge;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTel_no() {
+        return tel_no;
+    }
+
+    public void setTel_no(String tel_no) {
+        this.tel_no = tel_no;
+    }
+
+    public Integer getAvailable_storage() {
+        return available_storage;
+    }
+
+    public void setAvailable_storage(Integer available_storage) {
+        this.available_storage = available_storage;
     }
 
     public Ministry_Store_User getMinistry_store_incharge() {
