0d755f766f7d3027f63a0f7c74d6b16e3920b152
Kaumadie Kariyawasam <kkawmadie@gmail.com>
2020-05-30 18:37:44 +0530

    Create crud operation in rdhs hospital return drug

--- src/main/java/com/example/pharmanic/web/Rdhs_Hospital_Return_DrugController.java
+++ src/main/java/com/example/pharmanic/web/Rdhs_Hospital_Return_DrugController.java
@@ -4,22 +4,44 @@ import com.example.pharmanic.model.Rdhs_Hospital_Current_Stock;
 import com.example.pharmanic.model.Rdhs_Hospital_Return_Drug;
 
 import com.example.pharmanic.repositories.Rdhs_Hospital_Return_DrugRepository;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Collection;
 
 public class Rdhs_Hospital_Return_DrugController {
-    private Rdhs_Hospital_Return_DrugRepository rdhs_hospital_return_drug;
+    private Rdhs_Hospital_Return_DrugRepository rdhs_hospital_return_drugRepository;
 
-    public Rdhs_Hospital_Return_DrugController(Rdhs_Hospital_Return_DrugRepository rdhs_hospital_return_drug) {
+    public Rdhs_Hospital_Return_DrugController(Rdhs_Hospital_Return_DrugRepository rdhs_hospital_return_drugRepository) {
        super();
-        this.rdhs_hospital_return_drug = rdhs_hospital_return_drug;
+        this.rdhs_hospital_return_drugRepository = rdhs_hospital_return_drugRepository;
     }
 
     @GetMapping("/rhreturndrug")
     Collection<Rdhs_Hospital_Return_Drug> drugStore() {
 
-        return rdhs_hospital_return_drug.findAll();
+        return rdhs_hospital_return_drugRepository.findAll();
+    }
+
+    @PostMapping("/rhreturndrugs")
+    ResponseEntity<Rdhs_Hospital_Return_Drug> newReturnRecord(@Validated @RequestBody Rdhs_Hospital_Return_Drug rdhs_hospital_return_drug) throws URISyntaxException {
+        Rdhs_Hospital_Return_Drug result = rdhs_hospital_return_drugRepository.save(rdhs_hospital_return_drug);
+        return ResponseEntity.created(new URI("/api/rhreturndrugs" + result.getReturned_id())).body(result);
+    }
+
+
+    @PutMapping("/updatereturn{id}")
+    ResponseEntity<Rdhs_Hospital_Return_Drug> updateExpense(@Validated @RequestBody Rdhs_Hospital_Return_Drug rdhs_hospital_return_drug) {
+        Rdhs_Hospital_Return_Drug result = rdhs_hospital_return_drugRepository.save(rdhs_hospital_return_drug);
+        return ResponseEntity.ok().body(result);
+    }
+    @DeleteMapping("returnitem/{id}")
+    ResponseEntity<?> deleteCategory(@PathVariable Long id) {
+        rdhs_hospital_return_drugRepository.deleteById(id);
+        return ResponseEntity.ok().build();
     }
 
 }
