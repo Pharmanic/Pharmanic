98af2ead72a3f1250c065766dc088e59c171ab70
AnupamaTK <swathathsarani@gmail.com>
2020-08-26 17:29:24 +0530

    Revert "Update UserDetailsImpl.java"
    This reverts commit bf402212bbfc1f77b0bcf2c88c910137fb3977a7.

--- src/main/java/com/example/pharmanic/security/services/UserDetailsImpl.java
+++ src/main/java/com/example/pharmanic/security/services/UserDetailsImpl.java
@@ -25,17 +25,11 @@ public class UserDetailsImpl implements UserDetails {
     private String password;
 
 <<<<<<< HEAD
-<<<<<<< HEAD
     private String branch;
 
     private Collection<? extends GrantedAuthority> authorities;
 
     public UserDetailsImpl(Long id, String username, String email, String password,String branch,
-=======
-    private Collection<? extends GrantedAuthority> authorities;
-
-    public UserDetailsImpl(Long id, String username, String email, String password,
->>>>>>> parent of 6941f6b... Merge branch 'master' into Anupama
 =======
     private Collection<? extends GrantedAuthority> authorities;
 
@@ -48,10 +42,7 @@ public class UserDetailsImpl implements UserDetails {
         this.password = password;
         this.authorities = authorities;
 <<<<<<< HEAD
-<<<<<<< HEAD
         this.branch=branch;
-=======
->>>>>>> parent of 6941f6b... Merge branch 'master' into Anupama
 =======
 >>>>>>> parent of 6941f6b... Merge branch 'master' into Anupama
     }
@@ -67,13 +58,9 @@ public class UserDetailsImpl implements UserDetails {
                 user.getEmail(),
                 user.getPassword(),
 <<<<<<< HEAD
-<<<<<<< HEAD
                 user.getBranch(),
                 authorities
                 );
-=======
-                authorities);
->>>>>>> parent of 6941f6b... Merge branch 'master' into Anupama
 =======
                 authorities);
 >>>>>>> parent of 6941f6b... Merge branch 'master' into Anupama
@@ -93,15 +80,12 @@ public class UserDetailsImpl implements UserDetails {
     }
 
 <<<<<<< HEAD
-<<<<<<< HEAD
     public String getBranch() {
         return branch;
     }
 
 
 
-=======
->>>>>>> parent of 6941f6b... Merge branch 'master' into Anupama
 =======
 >>>>>>> parent of 6941f6b... Merge branch 'master' into Anupama
     @Override
