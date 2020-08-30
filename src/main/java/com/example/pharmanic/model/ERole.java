42816ba5e607d64cb1bd71af53c381e21b7c958a
AnupamaTK <swathathsarani@gmail.com>
2020-08-30 16:48:07 +0530

    Minor changes

--- src/main/java/com/example/pharmanic/model/ERole.java
+++ src/main/java/com/example/pharmanic/model/ERole.java
@@ -2,12 +2,8 @@ package com.example.pharmanic.model;
 
 public enum ERole {
     ROLE_USER,
-    ROLE_MODERATOR,
-    ROLE_ADMIN,
     ministry_admin,
-    ministry,
     minister,
-    rdhs_user,
     ministry_stock_keeper,
     ministry_store_admin,
     ministry_store_stock_keeper,
