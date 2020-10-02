755917e54862618ffc5167b79b6a38751b81cfce
kasuni perera <kasunilakshika96@gmail.com>
2020-10-02 09:14:33 +0530

    do some changes in backend

--- src/main/java/com/example/pharmanic/web/Rdhs_Request_OrderController.java
+++ src/main/java/com/example/pharmanic/web/Rdhs_Request_OrderController.java
@@ -25,6 +25,17 @@ public class Rdhs_Request_OrderController {
         return rdhs_request_orderService.getRDHSRequestOrderList();
     }
 
+
+    @GetMapping("/rdhsrequestordersnotcomplete")
+    public List<Rdhs_Request_Order> rdhsrequestordersnotcomplete(){
+        return rdhs_request_orderService.getnotcompleteRdhsRequestOrderList();
+    }
+
+    @GetMapping("/rdhspendingorders")
+    public List<Rdhs_Request_Order> rdhspendingorders(){
+        return rdhs_request_orderService.getRdhsPendingOrderList();
+    }
+
     @PostMapping("/rdhsrequestorder/add")
     public Integer addRdhsRequestOrder(@RequestBody Rdhs_Request_Order rdhs_request_order){
         return rdhs_request_orderService.addRdhsRequestOrder(rdhs_request_order);
