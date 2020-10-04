640a42366556da252e0d6f064d0b22c13264e509
AnupamaTK <swathathsarani@gmail.com>
2020-10-04 14:56:03 +0530

    User List done

---
+++ src/main/java/com/example/pharmanic/model/User_Detail_Interface.java
@@ -0,0 +1,10 @@
+package com.example.pharmanic.model;
+
+public interface User_Detail_Interface {
+
+    String getUserName();
+    String getEMail();
+    String getBranch();
+    String getName();
+
+}
