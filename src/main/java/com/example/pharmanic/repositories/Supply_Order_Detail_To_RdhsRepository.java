f1758d58c08147f5838501711510ffefaf77cb10
Anupama <swathathsarani@gmail.com>
2020-10-10 16:13:28 +0530

    Supply Demand Charts

--- src/main/java/com/example/pharmanic/repositories/Supply_Order_Detail_To_RdhsRepository.java
+++ src/main/java/com/example/pharmanic/repositories/Supply_Order_Detail_To_RdhsRepository.java
@@ -2,6 +2,18 @@ package com.example.pharmanic.repositories;
 
 import com.example.pharmanic.model.Supply_Order_Detail_To_Rdhs;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.sql.Date;
 
 public interface Supply_Order_Detail_To_RdhsRepository extends JpaRepository<Supply_Order_Detail_To_Rdhs,Long> {
+
+    @Query(value="SELECT sum(quantity) FROM supply_order_detail_to_rdhs where date = :date group by date",nativeQuery=true)
+    public Integer getDailySupplyToRDHS(@Param("date") Date date);
+
+    @Query(value="SELECT sum(quantity) FROM pharmanic.rdhs_request_order d inner join pharmanic.rdhs_request_order_detail dd on d.order_id=dd.order_id where date=:date group by date;",nativeQuery=true)
+    public Integer getDailyRequestsFromRDHS(@Param("date") Date date);
+
+
 }
--- src/main/java/com/example/pharmanic/web/Supply_Order_To_RdhsController.java
+++ src/main/java/com/example/pharmanic/web/Supply_Order_To_RdhsController.java
@@ -3,12 +3,12 @@ package com.example.pharmanic.web;
 import com.example.pharmanic.model.Supply_Order_To_Direct_Hospital;
 import com.example.pharmanic.model.Supply_Order_To_Rdhs;
 import com.example.pharmanic.repositories.Supply_Order_To_RdhsRepository;
+import com.example.pharmanic.services.Supply_Order_Service;
 import com.example.pharmanic.services.Supply_Order_To_RdhsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Date;
 
 @RestController
 @CrossOrigin(origins = "*",allowedHeaders = "*",maxAge = 3600)
@@ -19,10 +19,27 @@ public class Supply_Order_To_RdhsController {
 
     @Autowired
     Supply_Order_To_RdhsService supply_order_to_rdhsService;
+    @Autowired
+    Supply_Order_Service supplyOrderService;
 
     @PostMapping("/supplyordertordhs/add")
     public Integer addSupplyOrderTordhs(@RequestBody Supply_Order_To_Rdhs supply_order_to_rdhs){
         return supply_order_to_rdhsService.addSupplyOrderToRdhs(supply_order_to_rdhs);
+    }
+
+    @GetMapping("/getWeeklySupply")
+    public Integer[] getWeeklySupply(){
+        return supplyOrderService.getWeeklySupplyDetails();
+    }
+
+    @GetMapping("/getWeeklyDemand")
+    public Integer[] getWeeklyDemand(){
+        return supplyOrderService.getWeeklyDemandDetails();
+    }
+
+    @GetMapping("/getWeeklySupplyDates")
+    public Date[] getWeeklySupplyDates(){
+        return supplyOrderService.getWeeklySupplyDays();
     }
 
 
